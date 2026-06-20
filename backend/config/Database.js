b7e76f997098b7dfef40e37cbc2129c23f3dbd24
Ihsanrabani <ztex2727@gmail.com>
2026-06-20 20:52:44 +0700

    FE: favicon changed, npm run build | BE: fixing DB connection

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,17 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('sikasir_db', 'root', '', {
-        host: 'localhost',
-        dialect: 'mysql'
+const db_name = 'SiKasir_db_howevertip'
+const username = 'SiKasir_db_howevertip'
+const password = 'dacf30d611527059f3f4694530cc5a3b3b0cb40f'
+const host = 'o5shb8.h.filess.io'
+const port = '61002'
+const dialect = 'mysql'
+
+
+const db = new Sequelize(db_name, username, password, {
+        host: host,
+        dialect: dialect,
+        port: port
 })
 
 export default db;
